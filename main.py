f20aa5b9548e88f6acfa672dbfd75548a9f069a9
student <nikitamendukov@gmail.com>
2023-05-30 19:40:12 +0300

    bot

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import telebot
+from telebot import types
 from bs4 import BeautifulSoup
 import requests
-
+f = open('facts.txt', 'r', encoding='UTF-8')
+facts = f.read().split('\n')
+f.close()
+t = open('thinks.txt', 'r', encoding='UTF-8')
+thinks = t.read().split('\n')
+t.close()
 bot = telebot.TeleBot('5850667236:AAG1iZs_cECbmjzRwDyJLdfFGIYB38l9vCs')
-@bot.message_handlers(commands=['start'])
+@bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id,'Бомбооомэ🦍🦧')
-@bot.message_handlers(content_type=['text'])
-def handle_text(message):
-    bot.send_message(message.chat.id,'BimBim: ' + message.text)
+    bot.send_message(message.chat.id,'Бомбооом🦍🦧')
+@bot.message_handler(content_types=['text'])
+def handler_text(message):
+    bot.send_message(message.chat.id, 'BimBim: ' + message.text)
 bot.polling()
